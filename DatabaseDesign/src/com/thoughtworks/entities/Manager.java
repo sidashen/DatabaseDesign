6d1b521610979b4c190b45b723424ca9339cf4eb
sidashen <shashasi8023@163.com>
2020-03-06 22:16:06 +0800

    [shasha] feat: fields and constructors of manager class

--- DatabaseDesign/src/com/thoughtworks/entities/Manager.java
+++ DatabaseDesign/src/com/thoughtworks/entities/Manager.java
@@ -1,5 +1,24 @@
 package com.thoughtworks.entities;
 
 public class Manager {
+  int id;
+  String name;
+  int age;
+  String gender;
 
+  public Manager() {
+  }
+
+  public Manager(int id, String name, int age, String gender) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  @Override
+  public String toString() {
+    return "管理者编号：" + this.id + " 管理者姓名：" + this.name +
+      " 管理者年龄：" + this.age + " 管理者性别：" + this.gender;
+  }
 }
