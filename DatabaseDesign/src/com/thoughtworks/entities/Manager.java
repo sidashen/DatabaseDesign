cb6d5697b9cd687936dbe0044e5193b820ac254e
sidashen <shashasi8023@163.com>
2020-03-06 22:15:04 +0800

    [shasha] feat: fields and constructors of teacher class

--- DatabaseDesign/src/com/thoughtworks/entities/Manager.java
+++ DatabaseDesign/src/com/thoughtworks/entities/Manager.java
@@ -1,4 +1,5 @@
 package com.thoughtworks.entities;
 
 public class Manager {
+
 }
--- DatabaseDesign/src/com/thoughtworks/entities/Teacher.java
+++ DatabaseDesign/src/com/thoughtworks/entities/Teacher.java
@@ -3,6 +3,22 @@ package com.thoughtworks.entities;
 public class Teacher {
   int id;
   String name;
+  int age;
+  String gender;
 
+  public Teacher() {
+  }
 
+  public Teacher(int id, String name, int age, String gender) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  @Override
+  public String toString() {
+    return "教师编号：" + this.id + " 教师姓名：" + this.name +
+           " 教师年龄：" + this.age + " 教师性别：" + this.gender;
+  }
 }
