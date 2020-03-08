077afda22dc89844c1590e1102d9275564657242
sidashen <shashasi8023@163.com>
2020-03-08 19:05:38 +0800

    [shasha] refactor: jdbc query class

--- DatabaseDesign/src/com/thoughtworks/preparedstatement/crud/PreparedStatementQuery.java
+++ DatabaseDesign/src/com/thoughtworks/preparedstatement/crud/PreparedStatementQuery.java
@@ -17,11 +17,12 @@ public class PreparedStatementQuery {
 
   @Test
   public void test() {
-    String sql = "select id, name, age, gender from student_info where id < ?";
-    List<Student> list = queryInfoList(Student.class, sql, 20004);
+    String sql = "select id, name, age, gender from student_info where id = ?";
+    List<Student> list = queryInfoList(Student.class, sql, 20001);
     list.forEach(System.out::println);
   }
 
+  //多条记录
   public <T> List<T> queryInfoList(Class<T> clazz, String sql, Object ...args) {
     Connection conn = null;
     PreparedStatement ps = null;
@@ -63,4 +64,45 @@ public class PreparedStatementQuery {
     }
     return null;
   }
+
+  //单条记录
+  public <T> T queryInfo(Class<T> clazz, String sql, Object ...args) {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    try {
+      conn = JDBCUtils.getConnection();
+      ps = conn.prepareStatement(sql);
+      for (int i = 0; i < args.length; i++) {
+        ps.setObject(i + 1, args[i]);
+      }
+      rs = ps.executeQuery();
+      ResultSetMetaData rsmd = rs.getMetaData();
+      int columnCount = rsmd.getColumnCount();
+
+      if(rs.next()) {
+        T t = clazz.newInstance();
+        //处理结果集一行中的每一个列
+        for (int i = 0; i < columnCount; i ++) {
+          //获取列值
+          Object columnValue = rs.getObject(i + 1);
+          //获取每个列的列名
+          // String columnName = rsmd.getColumnName(i + 1);
+          //获取每个列的别名
+          String columnLabel = rsmd.getColumnLabel(i + 1);
+          //给score指定的属性赋值，通过反射
+          Field field = clazz.getDeclaredField(columnLabel);
+          field.setAccessible(true);
+          field.set(t, columnValue);
+        }
+        return t;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      JDBCUtils.closeResource(conn, ps, rs);
+    }
+    return null;
+  }
+
 }
