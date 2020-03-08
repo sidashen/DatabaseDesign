f51be2c2dd0629dc8d7b426d1d6e500b201bf45a
sidashen <shashasi8023@163.com>
2020-03-08 16:31:04 +0800

    [shasha] refactor: student class

--- DatabaseDesign/src/com/thoughtworks/entities/Student.java
+++ DatabaseDesign/src/com/thoughtworks/entities/Student.java
@@ -1,10 +1,10 @@
 package com.thoughtworks.entities;
 
 public class Student {
-  int id;
-  String name;
-  int age;
-  String gender;
+  private int id;
+  private String name;
+  private int age;
+  private String gender;
 
   public Student() {
   }
@@ -13,6 +13,38 @@ public class Student {
     this.id = id;
     this.name = name;
     this.age = age;
+    this.gender = gender;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
     this.gender = gender;
   }
 
