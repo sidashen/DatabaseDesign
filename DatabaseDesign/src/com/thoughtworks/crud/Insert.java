cf4a67da03ce4da61b331ca0fa0aa4c30288501b
sidashen <shashasi8023@163.com>
2020-03-09 02:21:59 +0800

    [shasha] feat: insert values into subject method

--- DatabaseDesign/src/com/thoughtworks/crud/Insert.java
+++ DatabaseDesign/src/com/thoughtworks/crud/Insert.java
@@ -1,8 +1,6 @@
 package com.thoughtworks.crud;
 
 import com.thoughtworks.preparedstatement.crud.PreparedStatementUpdate;
-import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -20,6 +18,20 @@ public class Insert {
         String gender = infoList.get(3).substring(4);
         String sql = "insert into student_info values (?, ?, ?, ?)";
         int insertCount = PreparedStatementUpdate.update(sql, id, name, age, gender);
+        if (insertCount > 0) {
+          System.out.println("添加成功");
+        } else {
+          System.out.println("添加失败");
+        }
+        break;
+      case "2.2":
+        System.out.println("请输入课程信息(例如：编号：1001, 名称: 计算机, 老师: 王老师)：");
+        infoList = Arrays.asList(scanner.nextLine().split(","));
+        int subjectId = Integer.parseInt(infoList.get(0).substring(3));
+        String subjectName = infoList.get(1).substring(4);
+        String teacherName = infoList.get(2).substring(4);
+        sql = "insert into subject_info values (?, ?, ?)";
+        insertCount = PreparedStatementUpdate.update(sql, subjectId, subjectName, teacherName);
         if (insertCount > 0) {
           System.out.println("添加成功");
         } else {
