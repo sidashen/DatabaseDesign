41c69e88be077c7a36108eb75691b056459c1cee
sidashen <shashasi8023@163.com>
2020-03-08 16:32:08 +0800

    [shasha] refactor: subject class

--- DatabaseDesign/src/com/thoughtworks/entities/Subject.java
+++ DatabaseDesign/src/com/thoughtworks/entities/Subject.java
@@ -1,9 +1,9 @@
 package com.thoughtworks.entities;
 
 public class Subject {
-  int subjectId;
-  String subjectName;
-  String teacherName;
+  private int subjectId;
+  private String subjectName;
+  private String teacherName;
 
   public Subject() {
   }
@@ -11,6 +11,30 @@ public class Subject {
   public Subject(int subjectId, String subjectName, String teacherName) {
     this.subjectId = subjectId;
     this.subjectName = subjectName;
+    this.teacherName = teacherName;
+  }
+
+  public int getSubjectId() {
+    return subjectId;
+  }
+
+  public void setSubjectId(int subjectId) {
+    this.subjectId = subjectId;
+  }
+
+  public String getSubjectName() {
+    return subjectName;
+  }
+
+  public void setSubjectName(String subjectName) {
+    this.subjectName = subjectName;
+  }
+
+  public String getTeacherName() {
+    return teacherName;
+  }
+
+  public void setTeacherName(String teacherName) {
     this.teacherName = teacherName;
   }
 
