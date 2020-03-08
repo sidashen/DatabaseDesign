db11d72263e8d6853a66859005a25443a6c5e861
sidashen <shashasi8023@163.com>
2020-03-08 16:30:24 +0800

    [shasha] refactor: manager class

--- DatabaseDesign/src/com/thoughtworks/entities/Manager.java
+++ DatabaseDesign/src/com/thoughtworks/entities/Manager.java
@@ -1,10 +1,10 @@
 package com.thoughtworks.entities;
 
 public class Manager {
-  int id;
-  String name;
-  int age;
-  String gender;
+  private int id;
+  private String name;
+  private int age;
+  private String gender;
 
   public Manager() {
   }
@@ -13,6 +13,38 @@ public class Manager {
     this.id = id;
     this.name = name;
     this.age = age;
+    this.gender = gender;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
     this.gender = gender;
   }
 
--- DatabaseDesign/src/com/thoughtworks/preparedstatement/crud/PreparedStatementCrud.java
+++ DatabaseDesign/src/com/thoughtworks/preparedstatement/crud/PreparedStatementCrud.java
@@ -24,5 +24,4 @@ public class PreparedStatementCrud {
     JDBCUtils.closeResource(conn, ps);
   }
 
-
 }
